050c0c09209d1ca45c925240e4f3a9635c3ecc0b
Indah Putri Rahmi Nurilah <131329672+Indahprn@users.noreply.github.com>
2023-12-05 12:15:16 +0700

    Add files via upload

---
+++ js-lanjutan/script.js
@@ -0,0 +1,48 @@
+// 2.1 EXECUTION CONTEXT, HOISTING & SCOPE
+
+// console.log(nama);
+// var nama = "Indah";
+
+// creation phase pada Global Context
+// nama var = undifined
+// nama functioon = fn()
+//hoisting
+
+// console.log(sayHello());
+
+// var nama = 'Indah';
+// var umur = '17';
+
+// function sayHello() {
+//     return(`Halo, nama saya ${nama}, saya ${umur} tahun.`);
+
+// }
+
+// var nama = "Indah";
+// var username = "@in_dahe";
+
+// function cetakURL(username) {
+//   console.log(arguments);
+//   var instagramURL = "http://instagram.com";
+//   return instagramURL + username;
+// }
+// 
+// console.log(cetakURL("@in_dahe"));
+
+// function a() {
+//     console.log('ini a');
+
+//     function b() {
+//         console.log('ini b');
+
+//         function c() {
+//             console.log('ini c');
+//         }
+
+//         c();
+//     }
+
+//     b();
+// }
+
+// a();
